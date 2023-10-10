c23d7a007d08140f6030307aeb3f2677aa061081
BADOUANA <badcedric25@gmail.com>
2023-10-10 10:04:43 +0200

    initial commit

---
+++ ratingA_back_end/src/main/java/com/epf/ratingA/RatingAApplication.java
@@ -0,0 +1,13 @@
+package com.epf.ratingA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingAApplication.class, args);
+	}
+
+}
---
+++ ratingA_back_end/src/test/java/com/epf/ratingA/RatingAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epf.ratingA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
