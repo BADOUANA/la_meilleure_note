0856b15741edd7e747aa95a142a5afc28f3f403b
BADOUANA <badcedric25@gmail.com>
2023-11-15 12:17:12 +0100

    erreur mapping

--- ratingA_back_end/src/main/java/com/epf/ratingA/services/FilmService.java
+++ ratingA_back_end/src/main/java/com/epf/ratingA/services/FilmService.java
@@ -2,48 +2,76 @@ package com.epf.ratingA.services;
 
 import com.epf.ratingA.dao.FilmDao;
 import com.epf.ratingA.dto.FilmDto;
+import com.epf.ratingA.exceptions.FilmException;
+import com.epf.ratingA.interfaces.IFilmServiceImpl;
 import com.epf.ratingA.mappers.FilmMapper;
 import com.epf.ratingA.models.Film;
 import com.epf.ratingA.models.Rate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Stream;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
-public class FilmService {
+public class FilmServiceImpl implements IFilmServiceImpl {
     private final FilmDao filmDao;
+    private final FilmMapper filmMapper;
 
+    @Override
     public List<Film> findAll(){return filmDao.findAll();}
+    @Override
     public List<Film> findAllBestFilmByRates(){return filmDao.getBestFilmByRates();}
+    @Override
     public List<Rate> findRatesByFilmId(Long id){return filmDao.getAllRatesFromFilm(id);}
+    @Override
     public Film findFilmByTitle(String title){return filmDao.findFilmByTitle(title);}
 
-    public Long createFilm(FilmDto filmDto){
+    @Override
+    public FilmDto createFilm(FilmDto filmDto) throws FilmException {
         try {
-            Film film = FilmMapper.INSTANCE.fromFilmDto(filmDto, null);
-            filmDao.save(film);
-            return film.getIdFilm();
+           /*Film film = filmMapper.filmDtoToFilm(filmDto);
+            Film filmCreated = filmDao.save(film);
+            return filmCreated.getIdFilm();*/
+                Film film = Film.builder()
+                        .idFilm(filmDto.getIdFilm())
+                        .title(filmDto.getTitle())
+                        .author(filmDto.getAuthor())
+                        .outDate(filmDto.getOutDate())
+
+                        .time(filmDto.getTime())
+                        .category(filmDto.getCategories())
+                        .build();
+                return Stream.of(filmDao.save(film)).map(filmMapper::filmToFilmDto).findFirst().get();
         }catch (Exception e){
             e.printStackTrace();
-            return null;
+            try {
+                throw new FilmException();
+            } catch (FilmException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
+    @Override
     public void updateFilm(FilmDto filmDto, Long filmId){
         filmDao.findById(filmId).orElseThrow(()->new NoSuchElementException("Film doesn't exist"));
         try {
-            Film film = FilmMapper.INSTANCE.fromFilmDto(filmDto, filmId);
-            filmDao.save(film);
+          /*  Film film = FilmMapper.INSTANCE.filmDtoToFilm(filmDto);
+            filmDao.save(film);*/
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
+    @Override
     public void deleteFilm(Long id){filmDao.deleteById(id);}
 
 
+    @Override
     public List<Film> searchFilmsByTitle(String searchTerm) {
         return filmDao.searchByTitle(searchTerm);
     }
